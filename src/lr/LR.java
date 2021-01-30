998c6b76efec6b905c83b7dc219a18a2558b42b9
Vandewiele Franck <franck.vandewiele@univ-littoral.fr>
2021-01-30 10:28:51 +0100

    Nettoyage

--- src/lr/LR.java
+++ src/lr/LR.java
@@ -11,8 +11,8 @@ import lr.format.simple.FormatSimple;
 class LR {
 	static final int LARGEUR = 1980;
 	static final int HAUTEUR = 1080;
-	static final int NBRAYONS = 500;
-	static final int NIVEAU = 12;
+	static final int NBRAYONS = 10;
+	static final int NIVEAU = 2;
 
 	public static void main(String[] args) {
 
